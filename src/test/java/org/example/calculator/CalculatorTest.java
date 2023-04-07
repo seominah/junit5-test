6df3f43be9f66010a97258943353d8e9e60a9d3c
seominah <seominah95@gmail.com>
2023-04-07 23:45:37 +0900

    Test: 사칙연산 테스트 코드 리팩토링

--- src/test/java/org/example/calculator/CalculatorTest.java
+++ src/test/java/org/example/calculator/CalculatorTest.java
@@ -2,24 +2,33 @@ package org.example.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CalculatorTest {
     // 1 + 2 ----> calculator
     //   3   <----
     @DisplayName("덧셈 연산을 수행한다.")
-    @Test
-    void additionTest() {
-        int result = Calculator.calculator(1, "+", 2);
-        assertThat(result).isEqualTo(3);
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void additionTest(int operand1, String operator, int operand2, int result) {
+        int calculatorResult = Calculator.calculator(operand1, operator, operand2);
+        assertThat(calculatorResult).isEqualTo(result);
     }
 
-    @DisplayName("뺄셈 연산을 수행한다.")
-    @Test
-    void subtractionTest() {
-        int result = Calculator.calculator(5, "-", 2);
-        assertThat(result).isEqualTo(3);
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(5, "-", 2, 3),
+                arguments(4, "*", 2, 8),
+                arguments(8, "/", 2, 4)
+        );
     }
 }
